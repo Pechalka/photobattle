f9efce9ac152baaaa9a4c95d973804df7be85893
Andrew <luzkovskyandrey@gmail.com>
2013-01-23 22:27:12 +0300

    base validation on registretion form

--- site/view_models/new_user.js
+++ site/view_models/new_user.js
@@ -1,26 +1,35 @@
-define(["knockout", "jquery", "qq"],
-    function(ko, $, qq) {
+define(["knockout", "jquery", "qq", "ko.mapping"],
+    function(ko, $, qq, mapping) {
         return function(model){
             var self = this;
 
-            self.user = {
+            self.user = mapping.fromJS({
                     name : '',
                     nick : '',
                     email : '',
                     password : '',
                     repeat_password : '',
                     description : '',
-                    type : ko.observable('amateur')
-                };
+                    type : 'amateur'
+                });
 
             self.save = function() {
-                $.post("/api/user", self.user, function() {
+                $.post("/api/user", ko.toJS(self.user), function() {
                     window.location = '#login_success';
                 });
 
                 return false;
             }
-           
+
+           self.agree_to_the_terms = ko.observable(false);
+
+            self.can_create = ko.computed(function(){
+             return   self.agree_to_the_terms() 
+                && self.user.nick() != ''
+                && self.user.email() != ''
+                && self.user.password() != ''
+                && self.user.password() == self.user.repeat_password();
+            });
         };
     }
 );
